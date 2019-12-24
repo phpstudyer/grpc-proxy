ad2cea7ca03ca33754928b6e0a536eeb34099664
zhaoyadong <250332754@qq.com>
2019-12-24 11:23:30 +0800

    update

--- handler_test.go
+++ handler_test.go
@@ -12,7 +12,8 @@ import (
 	"testing"
 	"time"
 
-	"github.com/mwitkow/grpc-proxy/proxy"
+	proxy "grpc-proxy"
+
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
@@ -24,7 +25,7 @@ import (
 
 	"fmt"
 
-	pb "github.com/mwitkow/grpc-proxy/testservice"
+	pb "grpc-proxy/testservice"
 )
 
 const (
